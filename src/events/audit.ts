3acdc2c1884169756e67e78cbd11c77abe62efb8
Fejtrix <samkodugas2902@gmail.com>
2026-07-21 17:46:05 +0200

    Refactor audit log handling and fix logging import

--- src/events/audit.ts
+++ src/events/audit.ts
@@ -1,33 +1,35 @@
 import { robloxClient, robloxGroup } from '../main';
-import { logAction } from '../handlers/handleLogging';
+import { logAction } from '../handlers/handlelogging';
 import { config } from '../config';
 
 let lastRecordedDate: number;
 
 const recordAuditLogs = async () => {
     try {
-    const auditLog = await robloxClient.apis.groupsAPI.getAuditLogs({
-        groupId: config.groupId,
-        actionType: 'ChangeRank',
-        limit: 10,
-        sortOrder: 'Desc',
-    });
+        const auditLog = await robloxClient.apis.groupsAPI.getAuditLogs({
+            groupId: config.groupId,
+            actionType: 'ChangeRank',
+            limit: 10,
+            sortOrder: 'Desc',
+        });
 
-    if (!auditLog.data || auditLog.data.length === 0) {
-        setTimeout(recordAuditLogs, 60000);
-        return;
-    }
+        if (!auditLog.data || auditLog.data.length === 0) {
+            setTimeout(recordAuditLogs, 60000);
+            return;
+        }
 
-    const mostRecentDate = new Date(auditLog.data[0].created).getTime();
+        const mostRecentDate = new Date(auditLog.data[0].created).getTime();
+        
+        if (lastRecordedDate) {
             const groupRoles = await robloxGroup.getRoles();
             auditLog.data.forEach(async (log) => {
-                if(robloxClient.user.id !== log.actor.user.userId) {
+                if (robloxClient.user.id !== log.actor.user.userId) {
                     const logCreationDate = new Date(log.created);
-                    if(Math.round(logCreationDate.getTime() / 1000) > Math.round(lastRecordedDate / 1000)) {
+                    if (Math.round(logCreationDate.getTime() / 1000) > Math.round(lastRecordedDate / 1000)) {
                         const oldRole = groupRoles.find((role) => role.id === log.description['OldRoleSetId']);
                         const newRole = groupRoles.find((role) => role.id === log.description['NewRoleSetId']);
                         const target = await robloxClient.getUser(log.description['TargetId']);
-                        logAction('Manual Set Rank', log.actor.user, null, target, `${oldRole.name} (${oldRole.rank}) → ${newRole.name} (${newRole.rank})`);
+                        logAction('Manual Set Rank', log.actor.user, null, target, `${oldRole.name} (${oldRole.rank}) -> ${newRole.name} (${newRole.rank})`);
                     }
                 }
             });
@@ -39,6 +41,6 @@ const recordAuditLogs = async () => {
         console.error(err);
     }
     setTimeout(recordAuditLogs, 60000);
-}
+};
 
 export { recordAuditLogs };
