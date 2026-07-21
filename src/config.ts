d0a16a31b344b267974113f71ddfe77ebac246cd
Fejtrix <samkodugas2902@gmail.com>
2026-07-21 17:06:01 +0200

    Update groupId in config to 169799474

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@ import { ActivityType } from 'discord.js';
 import { BotConfig } from './structures/types'; 
 
 export const config: BotConfig = {
-    groupId: 0,
+    groupId: 169799474,
     slashCommands: true,
     legacyCommands: {
         enabled: true,
